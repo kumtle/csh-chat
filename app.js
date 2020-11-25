68499518e159489d1cfd1dec0ca60b53c5b9a0d5
sangho <kumtle2@gmail.com>
2020-11-25 16:53:57 +0900

    proxy test

--- app.js
+++ app.js
@@ -1,99 +1,21 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var uuid = require('./utils/uuid');
-
-app.use(express.static(__dirname + '/static/'));
-
-app.get('/messenger', (req, res) => {
-    res.sendFile(__dirname + '/static/messenger.html');
+var http = require('http'),
+    httpProxy = require('http-proxy');
+ 
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+ 
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, { target: 'http://47.116.10.71:8080' });
 });
-
-app.get('/uuid', (req, res, next) => {
-    res.send(uuid.generate());
-});
-
-app.get('/notice', function(req, res, next) { 
-    res.send({response: 'complete'});
-});
-
-app.get('/', function(req, res, next) { 
-    //console.log(req);
-    var page = '/messenger';
-    var uri = req.protocol + '://' + req.get('host') + page;
-    var msg = 'If you want to test the messenger, go to this link <a href=page>uri</a>';
-    msg = msg.replace('page', page);
-    msg = msg.replace('uri', uri);
-
-    console.log(req.connection.localAddress);
-    console.log(req.connection.remoteAddress);
-    //console.log(server.address().address);
-    msg += '<br><br>localAddress : ' + req.connection.localAddress;
-    msg += '<br><br>remoteAddress : ' + req.connection.remoteAddress;
-    msg += '<br><br>ip : ' + server.address().address;
-    msg += '<br>port : ' + process.env.PORT;
-    
-    res.send(msg);
-});
-
-function isNullorEmpty(str){
-    return !str || str.length === 0;
-}
-
-var chat = io
-    .of('/chat')
-    .on('connection', (socket) => {
-
-        socket.on('login', data => {
-            try {
-                console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
-                console.log(data.name + ' joined Room \"' + data.room + '\"');
-
-                socket.name = data.name;
-                socket.userid = data.userid;
-                socket.room = data.room;
-                
-                socket.join(data.room);
-                // 전체에 알림
-                chat.to(data.room).emit('login', data.name);
-            }
-            catch(e) {
-                console.error(e);
-            }
-        });
-
-        socket.on('chat', data => {
-            if (!isNullorEmpty(socket.room) && socket.userid && data && data.msg) {
-                console.log('Message from %s %s', socket.name, data.msg);
-                var msg = {
-                    from: {
-                        name: socket.name,
-                        userid: socket.userid
-                    },
-                    msg: data.msg
-                };
-                socket.emit('mychat', msg);
-                socket.broadcast.emit('chat', msg);
-            }
-            else {
-                console.error('')
-            }
-        });
-
-        socket.on('notice', data => {
-            chat.emit('notice', "공지: message");
-        });
-
-        socket.on('forceDisconnect', () => {
-            socket.disconnect();
-        });
-
-        socket.on('disconnect', () => {
-            console.log('user disconnected: ' + socket.name);
-        })
-    });
-
-server.listen(process.env.PORT || 3000, () => {
-    console.log('Socket IO Server listening on port 3000');
-});
+ 
+//console.log("listening on port 5050")
+server.listen(process.env.PORT || 3000);
