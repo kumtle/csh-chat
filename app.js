9a508a789fcda0fc36d87e27153cd1c34ae6b769
sangho <kumtle2@gmail.com>
2021-05-07 19:21:03 +0900

    dd

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require('express');
-//const { request } = require('http');
-const http = require('http');
-const request = require('request');
 const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+const server = require('http').createServer(app);
+const corsOptions = {
+  //origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+  //origin: '*', // 모든 주소 허용함
+  origin: ['http://localhost:3000', 'http://localhost:2001', 'http://localhost:2000'] ,//'*', // 허락하고자 하는 요청 주소
+  credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+const io = require('socket.io')(server, { cors: corsOptions });
+const cors = require('cors');
 const uuid = require('./utils/uuid');
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer({});
+const port = process.env.PORT || 5000;
 
+// 라우팅
 app.use(express.static(__dirname + '/static/'));
-//app.use('/api', apiProxy);
+
+//app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/messenger', (req, res) => {
   res.sendFile(__dirname + '/static/messenger.html');
-});
-
-app.get('/sf', (req, res) => {
-  res.sendFile(__dirname + '/static/sketchfab.html');
 });
 
 app.get('/uuid', (req, res, next) => {
@@ -27,42 +29,50 @@ app.get('/uuid', (req, res, next) => {
 app.get('/notice', function(req, res, next) { 
   res.send({response: 'complete'});
 });
-/*
-app.get('/', (req, res, next) => {
-  proxy.web(req, res, { target: 'http://47.116.10.71:8080' });
-});*/
 
 app.get('/', function(req, res, next) { 
   //console.log(req);
   var page = '/messenger';
   var uri = req.protocol + '://' + req.get('host') + page;
-  var msg = 'If you want to test the messenger, go  to this link <a href=page>uri</a>';
+  var msg = 'If you want to test the messenger, go to this link <a href=page>uri</a>';
   msg = msg.replace('page', page);
   msg = msg.replace('uri', uri);
-  
-  console.log(req.connection.localAddress);
-  console.log(req.connection.remoteAddress);
-  //console.log(server.address().address);
-  msg += '<br><br>localAddress : ' + req.connection.localAddress;
-  msg += '<br><br>remoteAddress : ' + req.connection.remoteAddress;
-  msg += '<br><br>ip : ' + server.address().address;
-  msg += '<br>port : ' + process.env.PORT;
 
   res.send(msg);
 });
 
-function isNullorEmpty(str) {
-  return !str || str.length === 0;
-};
+//const event_connection = 'connection';
 
-app.get('/test', (req, res, next) => {
-  var uri = 'http://47.116.10.71:8080';
-  request(uri).pipe(res);
-});
+function checkLogin(data) {
+  if (data) {
+    return true;
+  }
+  return false;
+}
 
 var chat = io
   .of('/chat')
   .on('connection', (socket) => {
+    ///// New
+    socket.on('login_user', (data, callback) => {
+      if (checkLogin(data)) {
+        //// 로그인 처리
+        // user.add(uid, roomid, socketid);
+        //// 로그인시 기본 로비 입장
+        socket.join('lobby'); 
+        callback({result: 'complete', data: '로그인 성공'});
+      }
+      else {
+        callback({result: 'fail', data: '로그인 실패'});
+      }
+
+      if (callback) {
+        callback({ message: '로그인'});
+      }      
+    });
+
+
+    ///// Deprecated
     socket.on('login', data => {
       try {
         console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
@@ -81,6 +91,10 @@ var chat = io
       }
     });
 
+    socket.on('message', data => {
+
+    });
+
     socket.on('chat', data => {
       if (!isNullorEmpty(socket.room) && socket.userid && data && data.msg) {
         console.log('Message from %s %s', socket.name, data.msg);
@@ -92,7 +106,8 @@ var chat = io
           msg: data.msg
         };
         socket.emit('mychat', msg);
-        socket.broadcast.emit('chat', msg);
+        //console.log(socket.room);
+        socket.to(socket.room).emit('chat', msg);
       }
       else {
         console.error('');
@@ -107,11 +122,15 @@ var chat = io
       socket.disconnect();
     });
 
-    socket.on('disconnect', () => {
-      console.log('user disconnected: ' + socket.name);
+    socket.on('leaveRoom', () => {
+      socket.to(socket.room).emit('leaveRoom', socket.name)
     })
   });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Socket IO Server listening on port 3000');
+  function isNullorEmpty(str) {
+    return !str || str.length === 0;
+  };
+  
+server.listen(port, () => {
+  console.log('Socket IO Server listening on port %d', port);
 });
